e3a6c32b33d913f92668458efaa1d65681ca9787
FoxW13 <foxweymouth@gmail.com>
2024-09-25 18:13:38 -0500

    assets

---
+++ Prototype 3/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class PlayerController : MonoBehaviour
+{
+    private Rigidbody rb;
+    public float jumpForce;
+    public ForcedMode jumpForceMode;
+    public float gravityModifier;
+
+    public bool isOnGround = true;
+    public bool gameOver = false;
+
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+
+        Physics.gravity *= gravityModifier;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KayCode.Space) && isOnGround && !gameOver) 
+        {
+            rb.AddForce(Vector3.up * jumpForce, jumpForceMode);
+            isOnGround = false;
+        }
+    }
+}
