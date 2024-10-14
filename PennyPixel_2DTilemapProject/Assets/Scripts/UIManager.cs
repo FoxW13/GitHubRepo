4ca25511e06e6ea440d42d8b82707976fcfa29f9
FoxW13 <foxweymouth@gmail.com>
2024-10-13 21:13:35 -0500

    bruh

---
+++ PennyPixel_2DTilemapProject/Assets/Scripts/UIManager.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class UIManager : MonoBehaviour
+{
+    public static int score = 0;
+
+    public Text textbox;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+       
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!gameOver)
+        {
+            textbox.text = "Score: " + score;
+        }
+
+        if (gameOver)
+        {
+            if (won)
+            {
+                textbox.text = "You win!\nPress R to Try Again!";
+            }
+            else
+            {
+                textbox.text = "You lose!\nPress R to Try Again!";
+            }
+            if (Input.GetKeyDown(KeyCode.R))
+            {
+                SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+            }
+        }
+    }
+}
