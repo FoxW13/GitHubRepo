635b9f61ee6519378badf732f2aa7c177387b247
FoxW13 <foxweymouth@gmail.com>
2024-11-30 22:30:47 -0600

    should be done

---
+++ Prototype5/Assets/Scripts/DifficultyButton.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+/*
+		 * Fox Weymouth
+		 * Prototype 5
+		 * Assignment 8
+		 * This code controls the difficulty buttons in the menu. It starts the game with the difficutly
+		 * selected as well.
+		 */
+
+public class DifficultyButton : MonoBehaviour
+{
+
+    private Button button;
+
+    private GameManager gameManager;
+
+    public int difficulty;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        button = GetComponent<Button>();
+
+        button.onClick.AddListener(SetDifficulty);
+
+        gameManager = GameObject.FindGameObjectWithTag("GameManager").GetComponent<GameManager>();
+
+    }
+
+    void SetDifficulty()
+    {
+        Debug.Log(gameObject.name + " was clicked");
+        gameManager.StartGame(difficulty);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Prototype5/Assets/Scripts/Target.cs
@@ -0,0 +1,81 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/*
+		 * Fox Weymouth
+		 * Prototype 5
+		 * Assignment 8
+		 * This code runs the targets with their rotations, spawning locations, and speed. It also runs
+		 * the destrution of the target when clicked/out of bounds and ends the game with the "bad" target.
+		 */
+
+
+public class Target : MonoBehaviour
+{
+    private Rigidbody targetRb;
+
+    private float minSpeed = 12;
+    private float maxSpeed = 16;
+    private float maxTorque = 10;
+    private float xRange = 4;
+    private float ySpawnPos = -6;
+
+    private GameManager gameManager;
+
+    public int pointValue;
+
+    public ParticleSystem explosionParticle;
+    void Start()
+    {
+        targetRb = GetComponent<Rigidbody>();
+        targetRb.AddForce(RandomForce(), ForceMode.Impulse);
+        targetRb.AddTorque(RandomTorque(), RandomTorque(), RandomTorque(), ForceMode.Impulse);
+        transform.position = RandomSpawnPos();
+
+        gameManager = GameObject.FindGameObjectWithTag("GameManager").GetComponent<GameManager>();
+    }
+
+    private Vector3 RandomSpawnPos()
+    {
+        return new Vector3(Random.Range(-xRange, xRange), ySpawnPos);
+    }
+
+    private float RandomTorque()
+    {
+        return Random.Range(-maxTorque, maxTorque);
+    }
+
+    private Vector2 RandomForce()
+    {
+        return Vector2.up * Random.Range(minSpeed, maxSpeed);
+    }
+
+    private void OnMouseDown()
+    {
+        if (gameManager.isGameActive)
+        {
+            gameManager.UpdateScore(pointValue);
+
+            Instantiate(explosionParticle, transform.position, explosionParticle.transform.rotation);
+
+            Destroy(gameObject);
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (!gameObject.CompareTag("Bad"))
+        {
+            gameManager.GameOver();
+        }
+
+        Destroy(gameObject);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
