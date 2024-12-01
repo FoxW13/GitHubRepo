52b50664198e5da047794d77e51782162cdadf94
FoxW13 <foxweymouth@gmail.com>
2024-12-01 13:50:45 -0600

    done

---
+++ Challenge 5/Assets/Challenge 5/Scripts/DestroyObjectX.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/*
+         *Fox Weymouth
+         * Challenge 5
+		 * Assignment 8
+		 * This code runs the destroy function.
+		 */
+
+public class DestroyObjectX : MonoBehaviour
+{
+    void Start()
+    {
+        Destroy(gameObject, 2); // destroy particle after 2 seconds
+    }
+
+
+}
