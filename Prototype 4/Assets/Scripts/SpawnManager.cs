6f91344f85ba2aee3b6d23949a3876a2c284954e
FoxW13 <foxweymouth@gmail.com>
2024-12-11 17:35:38 -0600

    Update SpawnManager.cs

--- Prototype 4/Assets/Scripts/SpawnManager.cs
+++ Prototype 4/Assets/Scripts/SpawnManager.cs
@@ -33,11 +33,11 @@ public class SpawnManager : MonoBehaviour
         }
     }
 
-    private Vector3 GenerateSpawnPosition ()
+    private Vector3 GenerateSpawnPosition()
     {
         float spawnPosX = Random.Range(-spawnRange, spawnRange);
         float spawnPosZ = Random.Range(-spawnRange, spawnRange);
-        Vector3 randomPos = new Vector3(spawnPosX, 0 , spawnPosZ);
+        Vector3 randomPos = new Vector3(spawnPosX, 0, spawnPosZ);
         return randomPos;
     }
 
@@ -51,7 +51,7 @@ public class SpawnManager : MonoBehaviour
 
     void SpawnPowerup(int powerupsToSpawn)
     {
-        for (int i = 0;i < powerupsToSpawn; i++)
+        for (int i = 0; i < powerupsToSpawn; i++)
         {
             Instantiate(powerupPrefab, GenerateSpawnPosition(), powerupPrefab.transform.rotation);
         }
